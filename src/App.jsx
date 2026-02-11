564f8feb89e350677c974b0a5a30f500f2df82bc
DEV <DEV@NSN1191261202.edu119.local>
2026-02-11 16:04:29 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+export default function App() {
+  return (
+    <main>
+      <h1>Bem-vindo de volta</h1>
+      <p>Entre com seu e-mail e senha para continuar</p>
+
+      <input type="email" placeholder="E-mail" />
+      <input type="password" placeholder="Senha" />
+
+      <button>Entrar</button>
+    </main>
+  )
+}
